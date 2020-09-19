bbcf25c6fc36fabb124c1589d6644366ece303a0
Breno Souza <breno.vitoi@gmail.com>
2020-09-19 13:35:24 -0300

    DTO, Service

--- backend/src/main/java/com/daemoncyber/repositories/RecordRepository.java
+++ backend/src/main/java/com/daemoncyber/repositories/RecordRepository.java
@@ -1,9 +1,11 @@
 package com.daemoncyber.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.daemoncyber.entities.Game;
 
+@Repository
 public interface RecordRepository extends JpaRepository<Game, Long> {
 
 }
---
+++ backend/src/main/java/com/daemoncyber/resources/GameResource.java
@@ -0,0 +1,31 @@
+package com.daemoncyber.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daemoncyber.dto.GameDTO;
+import com.daemoncyber.entities.Game;
+import com.daemoncyber.services.GameService;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameResource {
+	
+	/*
+	 * @Autowired private GameRepository gameRepository;
+	 */
+	
+	@Autowired
+	private GameService service;
+	
+	@GetMapping
+	public ResponseEntity<List<GameDTO>> findAll() {
+		List<GameDTO> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+}
